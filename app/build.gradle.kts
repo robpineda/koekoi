da6197c37062d3e75a900bcadea2d7991de7cd9c
Roberto Pineda <rpinedan30@gmail.com>
2025-02-28 23:34:19 +0700

    "updated main menu and game activity"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.robertopineda.koekoi"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.robertopineda.koekoi"
--- app/src/main/java/com/robertopineda/koekoi/GameActivity.kt
+++ app/src/main/java/com/robertopineda/koekoi/GameActivity.kt
@@ -1,4 +1,26 @@
 package com.robertopineda.koekoi
 
-class GameActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class GameActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GameScreen()
+        }
+    }
+}
+
+@Composable
+fun GameScreen() {
+    Text(
+        text = "Welcome to KoeKoi's Game Mode",
+        modifier = Modifier.fillMaxSize()
+    )
 }
